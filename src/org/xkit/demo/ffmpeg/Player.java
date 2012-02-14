b5210929bd28e5a770f6fba08c9364df2e7c4bca
Hai <xscript@live.com>
2012-02-14 21:00:08 +0800

    init proj

---
+++ src/org/xkit/demo/ffmpeg/Player.java
@@ -0,0 +1,15 @@
+package org.xkit.demo.ffmpeg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Player extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
